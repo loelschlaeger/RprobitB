0b5cb8dacb6dda2ff0bba9f675799f72ef3a9110
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-06-16 11:37:02 +0200

    setup jss files

--- paper_jss/article.R
+++ paper_jss/article.R
@@ -8,45 +8,8 @@ library("MASS")
 
 
 ###################################################
-### code chunk number 2: data
+### code chunk number 2: test
 ###################################################
-data("quine", package = "MASS")
-
-
-###################################################
-### code chunk number 3: visualization
-###################################################
-par(mar = c(4, 4, 1, 1))
-plot(table(quine$Days), xlab = "Days", ylab = "Frequency", axes = FALSE)
-axis(2)
-axis(1, at = 0:16 * 5, labels = FALSE)
-axis(1, at = 0:8 * 10)
-box()
-
-
-###################################################
-### code chunk number 4: poisson
-###################################################
-m_pois <- glm(Days ~ (Eth + Sex + Age + Lrn)^2, data = quine,
-  family = poisson)
-
-
-###################################################
-### code chunk number 5: negbin
-###################################################
-library("MASS")
-m_nbin <- glm.nb(Days ~ (Eth + Sex + Age + Lrn)^2, data = quine)
-
-
-###################################################
-### code chunk number 6: comparison
-###################################################
-BIC(m_pois, m_nbin)
-
-
-###################################################
-### code chunk number 7: summary
-###################################################
-summary(m_nbin)
+x = 1
 
 
