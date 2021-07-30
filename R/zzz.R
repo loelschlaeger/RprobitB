a644eedc02ec5a3b6d681c8b96406e99cfcf7bf3
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2021-07-30 19:34:20 +0200

    updated startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 .onAttach = function(lib, pkg) {
   # startup message
   msg = c(paste0(
-    "RprobitB version ", packageVersion("RprobitB")),
-    "\nType 'citation(\"RprobitB\")' for citing this R package in publications.",
-    "\nSee https://github.com/loelschlaeger/RprobitB for references.")
+    "Thanks for installing 'RprobitB' version ", packageVersion("RprobitB")),
+    "\nType 'citation(\"RprobitB\")' for citing the R package in publications.",
+    "\n Happy choice modelling!")
   packageStartupMessage(msg)
   invisible()
 }
