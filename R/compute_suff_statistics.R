ffedef85192c6c6f43ff31472a50810861668cac
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2021-08-09 15:01:14 +0200

    put checks for scale in another function

--- R/compute_suff_statistics.R
+++ R/compute_suff_statistics.R
@@ -4,53 +4,20 @@
 #' @param data
 #' An object of class \code{RprobitB_data}, which can be obtained from
 #' \code{\link{simulate}} or \code{\link{prepare}}.
-#' @param scale
-#' A list of three elements, determining the parameter normalization with
-#' respect to the scale of utility:
-#' \itemize{
-#'   \item \code{parameter}:
-#'   either \code{"a"} (for a linear coefficient in \code{"alpha"}) or
-#'   \code{"s"} (for a variance in the error-term covariance matrix
-#'   \code{"Sigma"})
-#'   \item \code{index}:
-#'   the index of the parameter that gets fixed
-#'   \item \code{value}:
-#'   the value for the fixed parameter
-#' }
+#' @inheritParams check_scale
 #' @examples
 #' compute_suff_statistics(data = simulate(form = choice ~ var, N = 10, T = 10,
 #'                                         J = 3, re = "ASC"))
 #' @return
 #' A list of sufficient statistics
 
-compute_suff_statistics = function(data, scale = list("parameter" = "s",
-                                                      "index" = 1,
-                                                      "value" = 1)){
+compute_suff_statistics = function(data, scale){
 
   ### check input
   if(!inherits(data, "RprobitB_data"))
     stop("'data' must be of class 'RprobitB_data'.")
-  if(!is.list(scale))
-    stop("'scale' must be a list")
-  if(is.null(scale[["parameter"]]))
-    scale[["parameter"]] = "s"
-  if(!scale[["parameter"]] %in% c("s","a"))
-    stop("'scale$parameter' must be one of 'a' or 's'.")
-  if(is.null(scale[["index"]]))
-    scale[["index"]] = 1
-  if(scale[["parameter"]] == "a" &&
-     !scale[["index"]] %in% seq_len(data[["P_f"]]))
-    stop("'scale$index' is out of bound.")
-  if(scale[["parameter"]] == "s" &&
-     !scale[["index"]] %in% seq_len(data[["J"]]))
-    stop("'scale$index' is out of bound.")
-  if(is.null(scale[["value"]]))
-    scale[["value"]] = 1
-  if(!is.numeric(scale[["value"]]) || length(scale[["value"]])!=1 ||
-     scale[["value"]] == 0)
-    stop("'scale$value' must be a single numeric value not equal to zero.")
-  if(scale[["parameter"]] == "s" && scale[["value"]] < 0)
-    stop("'scale$value' must be non-negative.")
+  if(!inherits(scale, "RprobitB_scale"))
+    stop("'data' must be of class 'RprobitB_scale'.")
 
   ### extract parameters
   N = data$N
