ec856b8e655ae9868ef403223c26e9af8b48f424
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2021-08-09 15:01:34 +0200

    implemented model summary function

--- R/print.summary.RprobitB_model.R
+++ R/print.summary.RprobitB_model.R
@@ -1,5 +1,59 @@
 #' @export
 
 print.summary.RprobitB_model = function(x, ...) {
+
+  cat(paste0("Summary of model '",deparse1(x$form),"'\n\n"))
+
+  ### summary of model
+  cat("MCMC settings:\n")
+  cat("- R:",x$R,"\n")
+  cat("- B:",x$B,"\n")
+  cat("- Q:",x$Q,"\n")
+  cat("\n")
+
+  ### legend of covariates
+  cat("Legend of covariates:\n")
+  cat("\n")
+
+  ### get coefficient labels
+  labels = create_labels(P_f = x$P_f, P_r = x$P_r, J = x$J,
+                         C = x$latent_classes$C, C_est = x$statistics$C_est,
+                         symmetric = FALSE)
+
+  ### function that creates table format for estimates
+  print_tab = function(par_name){
+    true = as.vector(x$parm[[par_name]])
+    statistics = x$statistics[[par_name]]
+    if(x$simulated) statistics = cbind(true,statistics)
+    statistics = round(statistics, 2)
+    statistics = statistics[labels[[par_name]],,drop=FALSE]
+    colnames(statistics) = rep(sprintf("%6s"," "),ncol(statistics))
+    rownames(statistics) = sprintf("%6s",labels[[par_name]])
+    writeLines(paste("\n",par_name))
+    print(statistics)
+  }
+
+  cat("Posterior:\n")
+  cat(paste(sprintf("%6s"," "),
+            if(x$simulated) sprintf("%6s","true"),
+            sprintf("%6s","mean"),
+            sprintf("%6s","sd"),
+            sprintf("%6s","min"),
+            sprintf("%6s","q.25"),
+            sprintf("%6s","median"),
+            sprintf("%6s","q.75"),
+            sprintf("%6s","max"),
+            sprintf("%6s","R^")))
+
+  if(x$P_f>0)
+    print_tab(par_name = "alpha")
+  if(x$P_r>0){
+    print_tab(par_name = "s")
+    print_tab(par_name = "b")
+    print_tab(par_name = "Omega")
+  }
+  print_tab(par_name = "Sigma")
+
   return(invisible(x))
 }
+
--- R/summary.RprobitB_model.R
+++ R/summary.RprobitB_model.R
@@ -7,54 +7,24 @@
 #' description
 #' @export
 
-summary.RprobitB_model = function(object, digits = 2, ... ) {
+summary.RprobitB_model = function(object, ... ) {
 
   if(!inherits(object, "RprobitB_model"))
     stop("Not of class 'RprobitB_model'.")
-
-  ### extract parameters
-  simulated = object$RprobitB_data$simulated
-  statistics = object$statistics
-  true = object$RprobitB_data$parm
-
-  ### round statistics
-  statistics = lapply(statistics, function(x) round(x, digits = digits))
-
-  ### build matrix with estimates
-  est = data.frame()
-  if(object$RprobitB_data$P_f>0){
-    if(simulated){
-      est = rbind(est, cbind(true$alpha, statistics$alpha))
-    } else {
-      est = rbind(est, statistics$alpha)
-    }
-  }
-  if(object$RprobitB_data$P_r>0){
-    if(simulated){
-      est = rbind(est, cbind(true$s, statistics$s))
-      est = rbind(est, cbind(true$b, statistics$b))
-      est = rbind(est, cbind(true$Omega, statistics$Omega))
-    } else {
-      est = rbind(est, statistics$s)
-      est = rbind(est, statistics$b)
-      est = rbind(est, statistics$Omega)
-    }
-  }
-  if(simulated){
-    est = rbind(est, cbind(true$Sigma, statistics$Sigma))
-  } else {
-    est = rbind(est, statistics$Sigma)
-  }
 
   ### build 'summary.RprobitB_model' object
   out = list("form" = object$RprobitB_data$form,
              "R" = object$R,
              "B" = object$B,
              "Q" = object$Q,
+             "P_f" = object$RprobitB_data$P_f,
+             "P_r" = object$RprobitB_data$P_r,
+             "J" = object$RprobitB_data$J,
              "scale" = object$scale,
-             "lcus" = object$lcus,
+             "latent_classes" = object$latent_classes,
              "prior" = object$prior,
-             "estimates" = est,
+             "statistics" = object$statistics,
+             "simulated" = object$RprobitB_data$simulated,
              "parm" = object$RprobitB_data$parm)
 
   class(out) = "summary.RprobitB_model"
