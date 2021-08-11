d5ab74916a0449070248820f6a2ef1701a1e12d8
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2021-08-11 18:18:55 +0200

    removed redundant files

--- jss/workflow.R
+++
@@ -1,5 +0,0 @@
-try(setwd("paper_jss"),silent=TRUE)
-Sweave("rprobitb_oelschlaeger_bauer_jss_2021.Rnw")
-tools::texi2pdf("rprobitb_oelschlaeger_bauer_jss_2021.tex")
-Stangle("rprobitb_oelschlaeger_bauer_jss_2021.Rnw")
-
