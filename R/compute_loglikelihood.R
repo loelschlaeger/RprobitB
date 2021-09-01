93c26fba2737eeabec12554c16ea328b2d28c0b0
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2021-09-01 17:50:30 +0200

    Initialized function.

---
+++ R/compute_loglikelihood.R
@@ -0,0 +1,4 @@
+compute_loglikelihood = function() {
+
+
+}
---
+++ R/plot_estimates.R
@@ -0,0 +1,12 @@
+#' Visualizing the estimates.
+#' @description
+#' This function visualizes the parameter estimates and standard deviations.
+#' @inheritParams plot.RprobitB_model
+#' @return
+#' No return value. Draws a plot to the current device.
+
+plot_estimates = function(x, restrict = NULL, ...) {
+
+
+
+}
