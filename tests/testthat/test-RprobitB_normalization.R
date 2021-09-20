f72c280c879cfac5c5b1a37e729b9b94f73cb9e2
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2021-09-20 12:12:23 +0200

    Renamed RprobitB_scale -> RprobitB_normalization. Implemented print method.

---
+++ tests/testthat/test-RprobitB_normalization.R
@@ -0,0 +1,11 @@
+test_that("building of RprobitB_normalization works", {
+  J = sample(2:10,1)
+  P_f = sample(1:10,1)
+  expect_snapshot(RprobitB_normalization(J = J, P_f = P_f))
+  expect_error(RprobitB_normalization(J = J, P_f = P_f, level = J+1),
+               "'level' must be equal to 'J'.")
+  scale = list("parameter" = "a", "index" = 1, "value" = 1)
+  expect_snapshot(RprobitB_normalization(J = J, P_f = P_f, scale = scale))
+  expect_error(RprobitB_normalization(J = J, P_f = 0, scale = scale),
+               "Cannot use 'alpha' for normalization because the model has no fixed coefficients.")
+})
