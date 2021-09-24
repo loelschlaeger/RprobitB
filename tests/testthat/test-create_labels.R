84de1d481052fd9957a7e0f3863242cc0d88a053
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-09-24 15:26:06 +0200

    Implemented tests.

--- tests/testthat/test-create_labels.R
+++ tests/testthat/test-create_labels.R
@@ -1,6 +1,7 @@
 test_that("creation of labels works", {
   expect_snapshot(create_labels(P_f = 1, P_r = 2, J = 3, C = 4, symmetric = TRUE))
   expect_snapshot(create_labels(P_f = 1, P_r = 2, J = 3, C = 4, symmetric = FALSE))
-  expect_error(create_labels(P_f = 1, P_r = 2, J = 3, C = 0, symmetric = TRUE),
+  expect_snapshot(create_labels(P_f = 1, P_r = 0, J = 3, C = NA, symmetric = FALSE))
+  expect_error(create_labels(P_f = 1, P_r = 2, J = 3, C = NA, symmetric = TRUE),
                "'C' must be a number greater or equal 1.")
 })
