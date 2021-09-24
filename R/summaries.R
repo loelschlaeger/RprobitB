765c24ce05d8c35bba593d9ff0df4a061693d3e5
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-09-24 15:26:48 +0200

    Update of summaries.

--- R/summaries.R
+++ R/summaries.R
@@ -1,12 +1,13 @@
-#' Summary method for \code{RprobitB_data}
+#' Summary method for \code{RprobitB_data}.
 #' @param object
-#' An object of class \code{RprobitB_data}
+#' An object of class \code{RprobitB_data}.
 #' @param ...
-#' ignored
+#' Ignored.
 #' @export
 
 summary.RprobitB_data = function(object, ...){
 
+  ### check class of 'object'
   if(!inherits(object, "RprobitB_data"))
     stop("Not of class 'RprobitB_data'.")
 
@@ -73,23 +74,45 @@ summary.RprobitB_data = function(object, ...){
   return(out)
 }
 
-#' Summary method for \code{RprobitB_model}
+#' Summary method for \code{RprobitB_model}.
 #' @param object
 #' An object of class \code{RprobitB_model}.
 #' @param ...
-#' ignored
+#' Ignored.
 #' @export
 
 summary.RprobitB_model = function(object, ... ) {
 
-  ### check if 'object' is of class 'RprobitB_model'
+  ### check class of 'object'
   if(!inherits(object, "RprobitB_model"))
     stop("Not of class 'RprobitB_model'.")
 
+  ### determine estimated number of latent classes
+  if(object$latent_classes$update){
+    s_draws = object$gibbs_samples$gibbs_samples$s_draws
+    last_s_draw = tail(s_draws,1)
+    C_est = sum(last_s_draw != 0)
+  } else {
+    C_est = NA
+  }
+
   ### compute statistics from 'gibbs_samples'
+  if(object$latent_classes$update){
+    if(object$data$simulated){
+      C = max(object$latent_classes$Cmax, object$data$true_parameter$C)
+    } else {
+      C = object$latent_classes$Cmax
+    }
+  } else {
+    if(object$data$simulated){
+      C = max(object$latent_classes$C, object$data$true_parameter$C)
+    } else {
+      C = object$latent_classes$C
+    }
+  }
   statistics = compute_parameter_statistics(
-    gibbs_samples = gibbs_samples, P_f = data$P_f, P_r = data$P_r, J = data$J,
-    C = latent_classes$C)
+    gibbs_samples = object$gibbs_samples,
+    P_f = object$data$P_f, P_r = object$data$P_r, J = object$data$J, C = C)
 
   ### build 'summary.RprobitB_model' object
   out = list("form" = object$data$form,
@@ -98,14 +121,14 @@ summary.RprobitB_model = function(object, ... ) {
              "Q" = object$Q,
              "P_f" = object$data$P_f,
              "P_r" = object$data$P_r,
-             "cov_fix" = object$data$cov_fix,
-             "cov_random" = object$data$cov_random,
+             "covs" = object$data$covs,
              "J" = object$data$J,
              "alternatives" = object$data$alternatives,
-             "scale" = object$scale,
+             "normalization" = object$normalization,
              "latent_classes" = object$latent_classes,
              "prior" = object$prior,
-             "statistics" = object$statistics,
+             "statistics" = statistics,
+             "C_est" = C_est,
              "simulated" = object$data$simulated,
              "true_parameter" = object$data$true_parameter)
   class(out) = "summary.RprobitB_model"
