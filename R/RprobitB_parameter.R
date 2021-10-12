b38a63cfc5dfa8787304ff5e45a61ed22e7ec096
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2021-10-12 12:03:18 +0200

    Simulate parameters only on request.

--- R/RprobitB_parameter.R
+++ R/RprobitB_parameter.R
@@ -1,8 +1,8 @@
 #' Create object of class \code{RprobitB_parameter}.
 #' @description
 #' This function creates an object of class \code{RprobitB_parameter}.
-#' Missing parameters are sampled. All parameters are checked against the values
-#' of \code{P_f}, \code{P_r}, \code{J}, and \code{N}.
+#' If \code{sample = TRUE}, missing parameters are sampled. All parameters are
+#' checked against the values of \code{P_f}, \code{P_r}, \code{J}, and \code{N}.
 #' @inheritParams RprobitB_data
 #' @param alpha
 #' The fixed coefficient vector of length \code{P_f}.
@@ -27,7 +27,8 @@
 #' The error term covariance matrix of dimension \code{J} x \code{J}.
 #' Internally, \code{Sigma_full} gets differenced with respect to alternative
 #' \code{J}, so it becomes an identified covariance matrix of dimension
-#' \code{J-1} x \code{J-1}. If \code{Sigma} is specified, \code{Sigma_full} is ignored.
+#' \code{J-1} x \code{J-1}. If \code{Sigma} is specified, \code{Sigma_full} is
+#' ignored.
 #' @param beta
 #' The matrix of the decision-maker specific coefficient vectors of dimension
 #' \code{P_r} x \code{N}.
@@ -35,6 +36,8 @@
 #' @param z
 #' The vector of the allocation variables of length \code{N}.
 #' Set to \code{NA} if \code{P_r = 0}.
+#' @param sample
+#' A boolean, if \code{TRUE} missing parameters get sampled.
 #' @param seed
 #' Set a seed for sampling missing parameters.
 #' @return
@@ -42,29 +45,31 @@
 #' model parameters \code{alpha}, \code{C}, \code{s}, \code{b}, \code{Omega},
 #' \code{Sigma}, \code{Sigma_full}, \code{beta}, and \code{z}.
 
-RprobitB_parameter = function(P_f, P_r, J, N, alpha = NULL, C = NULL,
-                                   s = NULL, b = NULL, Omega = NULL,
-                                   Sigma = NULL, Sigma_full = NULL, beta = NULL,
-                                   z = NULL, seed = NULL) {
+RprobitB_parameter = function(P_f, P_r, J, N, alpha = NULL, C = NULL, s = NULL,
+                              b = NULL, Omega = NULL, Sigma = NULL,
+                              Sigma_full = NULL, beta = NULL, z = NULL, seed = NULL,
+                              sample = TRUE) {
 
   ### seed for sampling missing parameters
   if(!is.null(seed))
     set.seed(seed)
 
   ### alpha
-  if(P_f==0){
+  if(P_f == 0){
     alpha = NA
   } else {
-    if(!is.null(alpha)){
+    if(is.null(alpha) && !sample){
+      alpha = NA
+    } else {
+      if(is.null(alpha))
+        alpha = round(runif(P_f,-3,3),1)
       if(length(alpha)!=P_f || !is.numeric(alpha))
         stop("'alpha' must be a numeric vector of length ",P_f,".")
-    } else {
-      alpha = round(runif(P_f,-3,3),1)
     }
   }
 
   ### C, s, b, Omega, z, beta
-  if(P_r==0){
+  if(P_r == 0){
     C = NA
     s = NA
     b = NA
@@ -82,71 +87,95 @@ RprobitB_parameter = function(P_f, P_r, J, N, alpha = NULL, C = NULL,
     }
 
     ### s
-    if(is.null(s))
-      s = round(sort(as.vector(rdirichlet(rep(1,C))), decreasing = TRUE),2)
-    if(length(s)!=C || !is.numeric(s) || sum(s)!=1 || is.unsorted(rev(s)))
-      stop("'s' must be a non-ascending numeric vector of length ", C, " which sums up to 1.")
+    if(is.null(s) && !sample){
+      s = NA
+    } else {
+      if(is.null(s))
+        s = round(sort(as.vector(rdirichlet(rep(1,C))), decreasing = TRUE),2)
+      if(length(s)!=C || !is.numeric(s) || sum(s)!=1 || is.unsorted(rev(s)))
+        stop("'s' must be a non-ascending numeric vector of length ", C, " which sums up to 1.")
+    }
 
     ### b
-    if(is.null(b)){
-      b = matrix(0,nrow=P_r,ncol=C)
-      for(c in 1:C) b[,c] = round(runif(P_r,-3,3),1)
+    if(is.null(b) && !sample){
+      b = NA
+    } else {
+      if(is.null(b)){
+        b = matrix(0,nrow=P_r,ncol=C)
+        for(c in 1:C) b[,c] = round(runif(P_r,-3,3),1)
+      }
+      b = as.matrix(b)
+      if(!is.numeric(b) || nrow(b)!=P_r || ncol(b)!=C)
+        stop("'b' must be a numeric matrix of dimension ", P_r, " x ", C, ".")
     }
-    b = as.matrix(b)
-    if(!is.numeric(b) || nrow(b)!=P_r || ncol(b)!=C)
-      stop("'b' must be a numeric matrix of dimension ", P_r, " x ", C, ".")
 
     ### Omega
-    if(is.null(Omega)){
-      Omega = matrix(0,nrow=P_r*P_r,ncol=C)
+    if(is.null(Omega) && !sample){
+      Omega = NA
+    } else {
+      if(is.null(Omega)){
+        Omega = matrix(0,nrow=P_r*P_r,ncol=C)
+        for(c in 1:C)
+          Omega[,c] = as.vector(rwishart(P_r,diag(P_r))$W)
+      }
+      Omega = as.matrix(Omega)
+      if(!is.numeric(Omega) || nrow(Omega)!=P_r*P_r ||
+         ncol(Omega)!=C)
+        stop("'Omega' must be a numeric matrix of dimension ", P_r*P_r, " x ",
+             C, ".")
       for(c in 1:C)
-        Omega[,c] = as.vector(rwishart(P_r,diag(P_r))$W)
+        if(!is_covariance_matrix (matrix(Omega[,c],nrow=P_r,ncol=P_r)))
+          stop(paste("Column",c,"in 'Omega' builds no covariance matrix."))
     }
-    Omega = as.matrix(Omega)
-    if(!is.numeric(Omega) || nrow(Omega)!=P_r*P_r ||
-       ncol(Omega)!=C)
-      stop("'Omega' must be a numeric matrix of dimension ", P_r*P_r, " x ",
-           C, ".")
-    for(c in 1:C)
-      if(!is_covariance_matrix (matrix(Omega[,c],nrow=P_r,ncol=P_r)))
-        stop(paste("Column",c,"in 'Omega' builds no covariance matrix."))
 
     ### z
-    if(is.null(z))
-      z = sample(1:C, N, prob=s, replace=TRUE)
-    if(length(z)!=N || !is.numeric(z) || !all(z %in% 1:C))
-      stop("'z' must be a numeric vector of length ", N,
-           " with elements of value ", paste(seq_len(C), collapse = ", "), ".")
+    if(is.null(z) && !sample){
+      z = NA
+    } else {
+      if(is.null(z))
+        z = sample(1:C, N, prob=s, replace=TRUE)
+      if(length(z)!=N || !is.numeric(z) || !all(z %in% 1:C))
+        stop("'z' must be a numeric vector of length ", N,
+             " with elements of value ", paste(seq_len(C), collapse = ", "), ".")
+    }
 
     ### beta
-    if(is.null(beta)){
-      beta = matrix(0, nrow=P_r, ncol=N)
-      for(n in seq_len(N))
-        beta[,n] = b[,z[n]] +
-          t(chol(matrix(Omega[,z[n]], nrow=P_r, ncol=P_r))) %*% rnorm(P_r)
+    if(is.null(beta) && !sample){
+      beta = NA
+    } else {
+      if(is.null(beta)){
+        beta = matrix(0, nrow=P_r, ncol=N)
+        for(n in seq_len(N))
+          beta[,n] = b[,z[n]] +
+            t(chol(matrix(Omega[,z[n]], nrow=P_r, ncol=P_r))) %*% rnorm(P_r)
+      }
+      if(!is.numeric(beta) || nrow(beta)!=P_r ||
+         ncol(beta)!=N)
+        stop("'beta' must be a numeric matrix of dimension ", P_r, " x ", N, ".")
     }
-    if(!is.numeric(beta) || nrow(beta)!=P_r ||
-       ncol(beta)!=N)
-      stop("'beta' must be a numeric matrix of dimension ", P_r, " x ", N, ".")
-
   }
 
   ### Sigma
-  if(is.null(Sigma)){
-    if(is.null(Sigma_full)){
-      Sigma_full = rwishart(J,diag(J))$W
-    } else {
-      Sigma_full = as.matrix(Sigma_full)
-    }
-    Sigma = delta(J,J) %*% Sigma_full %*% t(delta(J,J))
+  if(is.null(Sigma_full) && is.null(Sigma) && !sample){
+    Sigma = NA
+    Sigma_full = NA
   } else {
-    Sigma = as.matrix(Sigma)
-    Sigma_full = undiff_Sigma(Sigma, i = J)
+    if(is.null(Sigma)){
+      if(is.null(Sigma_full)){
+        Sigma_full = rwishart(J,diag(J))$W
+      } else {
+        Sigma_full = as.matrix(Sigma_full)
+      }
+      Sigma = delta(J,J) %*% Sigma_full %*% t(delta(J,J))
+    } else {
+      Sigma = as.matrix(Sigma)
+      Sigma_full = undiff_Sigma(Sigma, i = J)
+    }
+    if(!(is_covariance_matrix(Sigma) && nrow(Sigma)==J-1))
+      stop("'Sigma' is not a proper (differenced) covariance matrix of dimension ", J-1, " x ", J-1, ".")
+    if(!(is_covariance_matrix(Sigma_full) && nrow(Sigma_full)==J))
+      stop("'Sigma_diff' is not a proper covariance matrix of dimension ", J, " x ", J, ".")
   }
-  if(!(is_covariance_matrix(Sigma) && nrow(Sigma)==J-1))
-    stop("'Sigma' is not a proper (differenced) covariance matrix of dimension ", J-1, " x ", J-1, ".")
-  if(!(is_covariance_matrix(Sigma_full) && nrow(Sigma_full)==J))
-    stop("'Sigma_diff' is not a proper covariance matrix of dimension ", J, " x ", J, ".")
 
   ### build and return 'RprobitB_parameter'-object
   out = list("alpha" = alpha,
