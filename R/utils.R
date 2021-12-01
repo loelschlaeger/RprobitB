900ba395275b8ff9e31fbe94b2e84c710c9b7b9f
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-12-01 10:56:07 +0100

    Reorderd utils function.

---
+++ R/utils.R
@@ -0,0 +1,192 @@
+#' Difference operator.
+#' @description
+#' This function computes the difference operator matrix for computing utility
+#' differences.
+#' @details
+#' Given a \code{J} x \code{P} matrix \code{X} of choice characteristics,
+#' then \code{delta(i,J)%*%X} computes differences with respect to alternative
+#' \code{i}.
+#' @param J
+#' The total number of alternatives.
+#' @param i
+#' The alternative number to which respect utility differences are computed.
+#' @return
+#' A matrix of dimension \code{J-1} x \code{J}.
+#' @export
+#' @keywords
+#' utils
+
+delta <- function(J, i) {
+  stopifnot(is.numeric(J), J %% 1 == 0, J >= 2)
+  stopifnot(is.numeric(i), i %% 1 == 0, i >= 1)
+  stopifnot(J >= 2)
+  stopifnot(J >= i)
+  if (i == 1) {
+    Delta <- cbind(-1, diag(J - 1))
+  } else if (i == J) {
+    Delta <- cbind(diag(J - 1), -1)
+  } else {
+    Delta <- cbind(diag(J - 1)[, 1:(i - 1)], -1, diag(J - 1)[, i:(J - 1)])
+  }
+  return(Delta)
+}
+
+#' Balancing visualization of multiple figures.
+#' @description
+#' This function finds a balanced setting for \code{par(mfrow)}.
+#' @param n
+#' The total number of figures.
+#' @return
+#' A vector of the form \code{c(nr,nc)}. If \code{par(mfrow = c(nr,nc))},
+#' subsequent figures will be drawn in an \code{nr} x \code{nc} array on the
+#' current device by rows.
+#' @export
+#' @keywords
+#' utils
+
+set_mfrow <- function(n) {
+  if (n == 1) {
+    return(c(1, 1))
+  }
+  ran <- 2:max(floor((n - 1) / 2), 1)
+  ran2 <- pmax(ceiling(n / (ran)), 1)
+  rem <- abs(n - ran2 * ran)
+  score <- abs(sqrt(n) - (ran)) + abs(sqrt(n) - (ran2)) + rem
+  nr <- ran[which.min(score)]
+  nc <- ran2[which.min(score)]
+  return(c(nr, nc))
+}
+
+
+#' Transform differenced to non-differenced error term covariance matrix.
+#' @description
+#' This function transforms the differenced error term covariance matrix
+#' \code{Sigma} back to a non-differenced error term covariance matrix.
+#' @param Sigma
+#' An error term covariance matrix of dimension \code{J-1} x \code{J-1} which
+#' was differenced with respect to alternative \code{i}.
+#' @param i
+#' An integer, the alternative number with respect to which \code{Sigma}
+#' was differenced.
+#' @return
+#' A covariance matrix of dimension \code{J} x \code{J}. If this covariance
+#' matrix gets differenced with respect to alternative \code{i}, the results is
+#' again \code{Sigma}.
+#' @keywords
+#' utils
+
+undiff_Sigma <- function(Sigma, i) {
+
+  ### check inputs
+  Sigma <- as.matrix(Sigma)
+  if (!is_covariance_matrix(Sigma)) {
+    stop("'Sigma' is no covariance matrix.")
+  }
+  J <- nrow(Sigma) + 1
+  if (!(length(i) == 1 && is.numeric(i) && i %% 1 == 0 && i <= J && i >= 1)) {
+    stop("'i' must an alternative number.")
+  }
+
+  ### add zero row and column to Sigma at row and column i
+  if (i == 1) {
+    Sigma_full <- rbind(0, cbind(0, Sigma))
+  } else if (i == J) {
+    Sigma_full <- rbind(cbind(Sigma, 0), 0)
+  } else {
+    Sigma_full <- cbind(Sigma[, 1:(i - 1)], 0, Sigma[, i:(J - 1)])
+    Sigma_full <- rbind(Sigma_full[1:(i - 1), ], 0, Sigma_full[i:(J - 1), ])
+  }
+
+  ### add kernel element to make all elements non-zero
+  Sigma_full <- Sigma_full + 1
+
+  ### check if 'Sigma_full' is a covariance matrix
+  if (!is_covariance_matrix(Sigma_full)) {
+    stop("Back-transformed matrix is no covariance matrix.")
+  }
+
+  ### check if back-differencing yields differenced matrix
+  Sigma_back <- delta(J, i) %*% Sigma_full %*% t(delta(J, i))
+  if (any(abs(Sigma_back - Sigma) > sqrt(.Machine$double.eps))) {
+    stop("Back-differencing failed.")
+  }
+
+  ### return undifferenced covariance matrix
+  names(Sigma_full) <- create_labels_Sigma(J + 1, cov_sym = TRUE)
+  return(Sigma_full)
+}
+
+#' Compute Gelman-Rubin statistic.
+#' @description
+#' This function computes the Gelman-Rubin statistic \code{R_hat}.
+#' @references
+#' <https://bookdown.org/rdpeng/advstatcomp/monitoring-convergence.html>
+#' @param samples
+#' A vector or a matrix of samples from a Markov chain, e.g. Gibbs samples.
+#' If \code{samples} is a matrix, each column gives the samples for a separate
+#' run.
+#' @param parts
+#' The number of parts to divide each chain into sub-chains.
+#' @return
+#' The Gelman-Rubin statistic.
+#' @examples
+#' no_chains <- 2
+#' length_chains <- 1e3
+#' samples <- matrix(NA, length_chains, no_chains)
+#' samples[1, ] <- 1
+#' Gamma <- matrix(c(0.8, 0.1, 0.2, 0.9), 2, 2)
+#' for (c in 1:no_chains) {
+#'   for (t in 2:length_chains) {
+#'     samples[t, c] <- sample(1:2, 1, prob = Gamma[samples[t - 1, c], ])
+#'   }
+#' }
+#' R_hat(samples)
+#' @export
+#' @keywords
+#' utils
+
+R_hat <- function(samples, parts = 2) {
+
+  ### divide chains into parts
+  samples <- as.matrix(samples)
+  no_chains <- ncol(samples)
+  length_chains <- nrow(samples)
+  sub_chains <- list()
+  for (c in 1:no_chains) {
+    sub_chains <- c(
+      sub_chains,
+      split(samples[, c], cut(1:length_chains, parts))
+    )
+  }
+
+  ### compute and return the Gelman-Rubin statistic
+  L <- length_chains / parts
+  chain_means <- sapply(sub_chains, mean)
+  grand_mean <- mean(chain_means)
+  B <- 1 / (parts - 1) * sum((chain_means - grand_mean)^2)
+  chain_variances <- sapply(sub_chains, var)
+  W <- sum(chain_variances) / parts
+  R_hat <- ((L - 1) / L * W + B) / W
+  return(R_hat)
+}
+
+#' Check covariance matrix properties.
+#' @description
+#' This function checks if the input is a proper covariance matrix, i.e.
+#' a symmetric, numeric matrix with non-negative eigenvalues.
+#' @param x
+#' A matrix.
+#' @return
+#' A boolean, \code{TRUE} if \code{x} is a proper covariance matrix.
+#' @export
+#' @keywords
+#' utils
+
+is_covariance_matrix <- function(x) {
+  is.matrix(x) &&
+    is.numeric(x) &&
+    ncol(x) == nrow(x) &&
+    all(abs(x - t(x)) < sqrt(.Machine$double.eps)) &&
+    all(eigen(x)$value > -sqrt(.Machine$double.eps))
+}
+
