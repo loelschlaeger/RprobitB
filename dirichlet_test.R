e706b430b8c1a0a550709459a3dc65b9fc3f9430
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-01-28 07:30:07 +0100

    Initialized dirichlet test.

---
+++ dirichlet_test.R
@@ -0,0 +1,11 @@
+### data generating process U = Xbeta + eps, beta ~ sum(s * normal), C fix, eps ~ standard_normal
+
+
+### estimate beta with LCMMNP approach
+
+
+
+### estimate beta with Dirichlet approach
+## updated class assignments
+## update mean and covariance in each class
+## draw each beta_c based on class assignment, mean and covariance of class based on Bayes regression formula
