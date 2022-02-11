a61ec8b9699c734e3d2eafdae3150228c8f6f715
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-02-11 14:35:12 +0100

    Revised creation of parameter labels.

--- R/parameter_labels.R
+++ R/parameter_labels.R
@@ -1,36 +1,37 @@
-#' Create parameters labels.
+#' Create parameters labels
 #'
 #' @description
-#' This function creates labels for the model parameters.
+#' This function model parameter labels.
 #'
 #' @inheritParams RprobitB_data
-#' @param C
-#' The number of latent classes.
 #' @param cov_sym
-#' If \code{TRUE} considers symmetric covariance matrix elements.
+#' Set to \code{TRUE} for labels of symmetric covariance elements.
 #' @param keep_par
 #' A vector of parameter names which are kept.
 #' @param drop_par
 #' A vector of parameter names which get dropped.
 #'
 #' @return
-#' A list of labels for the model parameters.
+#' A list of labels for the selected model parameters.
+#'
+#' @examples
+#' parameter_labels()
 #'
 #' @keywords
 #' internal
 
-parameter_labels <- function(P_f, P_r, J, C, cov_sym,
+parameter_labels <- function(P_f, P_r, J, N, C, cov_sym,
                              keep_par = c("s", "alpha", "b", "Omega", "Sigma"),
                              drop_par = NULL) {
 
-  ### check that 'C' is a number if 'P_r>0'
+  ### check inputs
   if (P_r > 0) {
     if (!(is.numeric(C) && C %% 1 == 0 && C >= 1)) {
       stop("'C' must be a number greater or equal 1.")
     }
   }
 
-  ### create and return list of labels
+  ### build labels
   labels <- list(
     "s" = create_labels_s(P_r, C),
     "alpha" = create_labels_alpha(P_f),
@@ -38,87 +39,98 @@ parameter_labels <- function(P_f, P_r, J, C, cov_sym,
     "Omega" = create_labels_Omega(P_r, C, cov_sym),
     "Sigma" = create_labels_Sigma(J, cov_sym)
   )
-  labels <- labels[lengths(labels) != 0 &
-    names(labels) %in% keep_par &
-    !names(labels) %in% drop_par]
+
+  ### filter and return labels
+  labels <- labels[lengths(labels) != 0 & names(labels) %in% keep_par & !names(labels) %in% drop_par]
   return(labels)
 }
 
-#' Create labels for \code{alpha}.
-#'
-#' @description
-#' This function creates labels for the model parameter \code{alpha}.
-#'
-#' @inheritParams parameter_labels
-#'
-#' @return
-#' A vector of labels for the model parameter \code{alpha}.
-#'
-#' @keywords
-#' internal
-
-create_labels_alpha <- function(P_f) {
-  if (P_f == 0) NULL else as.character(seq_len(P_f))
-}
-
-#' Create labels for \code{s}.
-#'
+#' Create labels for \code{s}
 #' @description
 #' This function creates labels for the model parameter \code{s}.
-#'
 #' @inheritParams parameter_labels
-#'
 #' @return
-#' A vector of labels for the model parameter \code{s}.
-#'
+#' A vector of labels for the model parameter \code{s} of length \code{C} if
+#' \code{P_r > 0} and \code{NULL} otherwise.
+#' @examples
+#' RprobitB:::create_labels_s(1,3)
 #' @keywords
 #' internal
 
 create_labels_s <- function(P_r, C) {
-  if (P_r == 0) NULL else as.character(seq_len(C))
+  if (P_r > 0) {
+    as.character(seq_len(C))
+  } else {
+    NULL
+  }
 }
 
-#' Create labels for \code{b}.
-#'
+#' Create labels for \code{alpha}
+#' @description
+#' This function creates labels for the model parameter \code{alpha}.
+#' @inheritParams parameter_labels
+#' @return
+#' A vector of labels for the model parameter \code{alpha} of length \code{P_f}
+#' if \code{P_f > 0} and \code{NULL} otherwise.
+#' @examples
+#' RprobitB:::create_labels_alpha(P_f = 3)
+#' @keywords
+#' internal
+
+create_labels_alpha <- function(P_f) {
+  if (P_f > 0) {
+    as.character(seq_len(P_f))
+  } else {
+    NULL
+  }
+}
+
+#' Create labels for \code{b}
 #' @description
 #' This function creates labels for the model parameter \code{b}.
-#'
+#' @details
+#' The labels are of the form \code{"c.p"}, where \code{c} is the latent class
+#' number and \code{p} the index of the random coefficient.
 #' @inheritParams parameter_labels
-#'
 #' @return
-#' A vector of labels for the model parameter \code{b}.
-#'
+#' A vector of labels for the model parameter \code{b} of length \code{P_r * C}
+#' if \code{P_r > 0} and \code{NULL} otherwise.
+#' @examples
+#' RprobitB:::create_labels_b(2,3)
 #' @keywords
 #' internal
 
 create_labels_b <- function(P_r, C) {
-  if (P_r == 0) {
-    NULL
-  } else {
+  if (P_r > 0) {
     paste0(
       as.character(rep(1:C, each = P_r)), rep(".", P_r * C),
       as.character(rep(1:P_r, times = C))
     )
+  } else {
+    NULL
   }
 }
 
-#' Create labels for \code{Omega}.
-#'
+#' Create labels for \code{Omega}
 #' @description
 #' This function creates labels for the model parameter \code{Omega}.
-#'
+#' @details
+#' The labels are of the form \code{"c.p1,p2"}, where \code{c} is the latent class
+#' number and \code{p1,p2} the indeces of two random coefficients.
 #' @inheritParams parameter_labels
-#'
 #' @return
-#' A vector of labels for the model parameter \code{Omega}.
-#'
+#' A vector of labels for the model parameter \code{Omega} of length
+#' \code{P_r^2 * C} if \code{P_r > 0} and \code{cov_sym = TRUE}
+#' or of length \code{P_r*(P_r+1)/2*C} if \code{cov_sym = FALSE} and \code{NULL}
+#' otherwise.
+#' @examples
+#' RprobitB:::create_labels_Omega(2, 3, cov_sym = TRUE)
+#' RprobitB:::create_labels_Omega(2, 3, cov_sym = FALSE)
 #' @keywords
 #' internal
 
 create_labels_Omega <- function(P_r, C, cov_sym) {
-  if (P_r == 0) {
-    NULL
-  } else {
+  if (P_r > 0) {
     Omega_id <- rep(TRUE, P_r * P_r)
     if (!cov_sym) {
       Omega_id[-which(lower.tri(matrix(NA, P_r, P_r), diag = TRUE) == TRUE)] <- FALSE
@@ -131,19 +143,25 @@ create_labels_Omega <- function(P_r, C, cov_sym) {
         rep(1:P_r, times = P_r)
       ), times = C))
     )[Omega_id]
+  } else {
+    NULL
   }
 }
 
-#' Create labels for \code{Sigma}.
-#'
+#' Create labels for \code{Sigma}
 #' @description
 #' This function creates labels for the model parameter \code{Sigma}.
-#'
+#' @details
+#' The labels are of the form \code{"j1,j2"}, where \code{j1,j2} are indices
+#' of two alternatives.
 #' @inheritParams parameter_labels
-#'
 #' @return
-#' A vector of labels for the model parameter \code{Sigma}.
-#'
+#' A vector of labels for the model parameter \code{Sigma} of length
+#' \code{(J-1)^2} if \code{cov_sym = TRUE} or of length \code{P_r*(P_r+1)/2*C}
+#' if \code{cov_sym = FALSE}.
+#' @examples
+#' RprobitB:::create_labels_Sigma(3, cov_sym = TRUE)
+#' RprobitB:::create_labels_Sigma(4, cov_sym = FALSE)
 #' @keywords
 #' internal
 
