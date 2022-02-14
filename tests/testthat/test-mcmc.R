912579fca1ac32e7d6b55a3895f8c4c02e2f1e28
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-02-14 18:47:03 +0100

    Test update.

--- tests/testthat/test-mcmc.R
+++ tests/testthat/test-mcmc.R
@@ -8,7 +8,7 @@ test_that("P", {
     seed = 1,
     alpha = 1:5, Sigma = 1
   )
-  model <- mcmc(data, R = 1000, print_progress = FALSE, seed = 1)
+  model <- mcmc(data, R = 2000, print_progress = FALSE, seed = 1)
   expect_snapshot(print(model))
   expect_snapshot(summary(model))
 })
@@ -57,14 +57,14 @@ test_that("LCMMNP", {
     C = 2
   )
   model <- mcmc(data,
-    R = 1000, print_progress = FALSE, seed = 1,
+    R = 500, print_progress = FALSE, seed = 1,
     latent_classes = list("C" = 2)
   )
   expect_snapshot(print(model))
   expect_snapshot(summary(model))
 })
 
-test_that("ULCMMNP", {
+test_that("LCMMNP weight update", {
   data <- simulate_choices(
     form = choice ~ cost,
     N = 100,
@@ -76,10 +76,52 @@ test_that("ULCMMNP", {
     C = 2
   )
   model <- mcmc(data,
-    R = 2000, print_progress = T, seed = 1,
+    R = 500, print_progress = FALSE, seed = 1,
     latent_classes = list(
       "C" = 8, "weight_update" = TRUE, "epsmin" = 0.1, "epsmax" = 0.9
     )
+  )
+  expect_snapshot(print(model))
+  expect_snapshot(summary(model))
+})
+
+test_that("LCMMNP DP update", {
+  data <- simulate_choices(
+    form = choice ~ cost,
+    N = 100,
+    T = 10,
+    J = 3,
+    re = c("cost", "ASC"),
+    alternatives = c("train", "bus", "car"),
+    seed = 1,
+    C = 2
+  )
+  model <- mcmc(data,
+                R = 500, print_progress = FALSE, seed = 1,
+                latent_classes = list(
+                  "C" = 8, "Cmax" = 10, "dp_update" = TRUE
+                )
+  )
+  expect_snapshot(print(model))
+  expect_snapshot(summary(model))
+})
+
+test_that("LCMMNP weight and DP update", {
+  data <- simulate_choices(
+    form = choice ~ cost,
+    N = 100,
+    T = 10,
+    J = 3,
+    re = c("cost", "ASC"),
+    alternatives = c("train", "bus", "car"),
+    seed = 1,
+    C = 2
+  )
+  model <- mcmc(data,
+                R = 500, print_progress = FALSE, seed = 1,
+                latent_classes = list(
+                  "C" = 8, "weight_update" = TRUE, "dp_update" = TRUE, "epsmin" = 0.1, "epsmax" = 0.9
+                )
   )
   expect_snapshot(print(model))
   expect_snapshot(summary(model))
