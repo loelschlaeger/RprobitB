14773ba1d2d4640bcfef3f0a224f5130f6499470
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-02-21 09:32:45 +0100

    Initialized demo function.

---
+++ R/RprobitB_demo.R
@@ -0,0 +1,16 @@
+#' A demonstration of RprobitB
+#'
+#' @description
+#' This function demonstrates the functionality of RprobitB.
+#'
+#' @export
+
+RprobitB_demo <- function() {
+  warning("Not yet implemented.")
+}
+
+#' @noRd
+
+rpb <- function() {
+  .Deprecated("RprobitB_demo")
+}
