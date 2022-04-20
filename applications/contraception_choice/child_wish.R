ea9868eac436e611a52b92425b289abc4917fbc7
Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2022-04-21 00:24:09 +0200

    Update.

--- applications/contraception_choice/child_wish.R
+++ applications/contraception_choice/child_wish.R
@@ -14,6 +14,6 @@ plot(data_cw, by_choice = TRUE)
 saveRDS(data_cw, "applications/contraception_choice/data_cw.rds", compress = "xz")
 
 model_child_wish <- mcmc(data = data_cw,
-                         prior = list("delta" = 0.1),
-                         latent_classes = list("C" = 10, "dp_update" = TRUE))
+                         prior = list("delta" = 0.01),
+                         latent_classes = list("C" = 5, "dp_update" = TRUE))
 saveRDS(model_child_wish, "applications/contraception_choice/model_child_wish.rds", compress = "xz")
