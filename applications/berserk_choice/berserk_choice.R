65a1d7706aa7ccad9c5504624ce3cc52491b317b
Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2022-05-10 10:13:43 +0200

    Estimated models.

--- applications/berserk_choice/berserk_choice.R
+++ applications/berserk_choice/berserk_choice.R
@@ -8,7 +8,7 @@ choice_berserk <- create_lagged_cov(
 
 data <- prepare_data(
   form = berserk ~ 0 | white + rating + rating_diff + min_rem + streak + berserk.1 + lost.1 + 1,
-  re = c("rating","min_rem","lost.1"),
+  re = c("rating_diff","lost.1"),
   choice_data = choice_berserk,
   id = "player_id",
   idc = "game_id",
@@ -19,7 +19,12 @@ data <- prepare_data(
 plot(data, by_choice = TRUE)
 saveRDS(data, "applications/berserk_choice/data.rds", compress = "xz")
 
-model_berserk <- fit_model(data)
+model_berserk <- fit_model(data,
+                           latent_classes = list("dp_update" = TRUE, "C" = 10),
+                           R = 5000)
 saveRDS(model_berserk, "applications/berserk_choice/model_berserk.rds", compress = "xz")
 
 coef(model_berserk)
+summary(model_berserk)
+plot(model_berserk, "mixture")
+plot(model_berserk, "class_seq")
