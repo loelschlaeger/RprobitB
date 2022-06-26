641234e4f8b169f6daba7206a91429f2b9671ce8
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-06-26 16:22:02 +0200

    Updated assets.

--- R/models.R
+++ R/models.R
@@ -90,3 +90,85 @@
 #' @keywords
 #' model
 "model_elec"
+
+#' Latent class mixed probit model for binary berserking choice
+#'
+#' @description
+#' This object is a fitted mixed probit model to the \code{choice_berserk}
+#' dataset of the {RprobitB} package with the formula
+#' `berserk ~ 0 | white + rating + rating_diff + min_rem + streak + berserk.1 + lost.1 + 1`.
+#'
+#' @usage data(model_berserk)
+#'
+#' @details
+#' The model was estimated via
+#' \preformatted{
+#' choice_berserk <- create_lagged_cov(
+#'   choice_data = RprobitB::choice_berserk,
+#'   column = c("berserk","lost"),
+#'   k = 1,
+#'   id = "player_id"
+#' )
+#'
+#' data_berserk <- prepare_data(
+#'   form = berserk ~ 0 | white + rating + rating_diff + min_rem + streak + berserk.1 + lost.1 + 1,
+#'   re = c("rating_diff","lost.1"),
+#'   choice_data = choice_berserk,
+#'   id = "player_id",
+#'   idc = "game_id",
+#'   standardize = c("rating","rating_diff","min_rem"),
+#'   impute = "zero"
+#' )
+#' model_berserk <- fit_model(
+#'   data_berserk,
+#'   latent_classes = list("dp_update" = TRUE, "C" = 10),
+#'   R = 5000
+#' )
+#' }
+#'
+#' @format An object of class \code{RprobitB_fit}.
+#'
+#' @keywords
+#' model
+"model_berserk"
+
+#' Latent class mixed probit model for chess opening choice
+#'
+#' @description
+#' This object is a fitted mixed probit model to the \code{choice_chess_opening}
+#' dataset of the {RprobitB} package with the formula
+#' `w1 ~ w1.1 | sex_w + byear_w + rating_w + rating_diff + 1`.
+#'
+#' @usage data(model_chessop)
+#'
+#' @details
+#' The model was estimated via
+#' \preformatted{
+#' choice_chess_opening <- create_lagged_cov(
+#'   choice_data = RprobitB::choice_chess_opening,
+#'   column = "w1",
+#'   k = 1,
+#'   id = "fideid_w")
+#' choice_chess_opening <- fastDummies::dummy_cols(choice_chess_opening, "w1.1")
+#' choice_chess_opening$rating_diff <- choice_chess_opening$rating_w - choice_chess_opening$rating_b
+#' data <- prepare_data(
+#'   form = w1 ~ w1.1 | sex_w + byear_w + rating_w + rating_diff + 1,
+#'   choice_data = choice_chess_opening,
+#'   id = "fideid_w",
+#'   idc = "fideid_b",
+#'   alternatives = c("e4","d4","b3"),
+#'   standardize = c("byear_w","rating_w","rating_diff"),
+#'   impute = "complete_cases"
+#' )
+#' model_chessop <- fit_model(
+#'   data = data,
+#'   re = "w1.1",
+#'   latent_classes = list("dp_update" = TRUE, "C" = 2)
+#' )
+#' }
+#'
+#' @format An object of class \code{RprobitB_fit}.
+#'
+#' @keywords
+#' model
+"model_chessop"
