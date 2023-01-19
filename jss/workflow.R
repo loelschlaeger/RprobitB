dd852a52b14a0697a39cb90ea159052eb2b3fb6a
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2023-01-19 16:12:34 +0100

    deleted jss files (separate repo now)

--- jss/workflow.R
+++
@@ -1,4 +0,0 @@
-try(setwd("jss"),silent=TRUE)
-Sweave("rprobitb_oelschlaeger_bauer.Rnw", encoding = "utf8")
-tinytex::pdflatex("rprobitb_oelschlaeger_bauer.tex")
-# Stangle("rprobitb_oelschlaeger_bauer.Rnw", encoding = "utf8")
